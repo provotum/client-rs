63a64bd22fb22f00b1d5e13a1d18c360a76b6a7e
Raphael Matile <raphael.matile@gmail.com>
2018-10-01 10:46:45 +0200

    Add docs

---
+++ docs/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["client_rs"] = {"doc":"Client to submit a vote. Requires a public_key.json, a private_uciv.json and a public_uciv.json in the project root. These can be generated using generator-rs.","items":[],"paths":[]};
+initSearch(searchIndex);
