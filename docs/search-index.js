824b5f9cdb247fce784a1b841f5a32d8ca3810ec
Raphael Matile <raphael.matile@gmail.com>
2018-10-06 18:29:38 +0200

    Add modified binary and updated docs

--- docs/search-index.js
+++ docs/search-index.js
@@ -1,3 +1,3 @@
 var searchIndex = {};
-searchIndex["client_rs"] = {"doc":"Client to submit a vote. Requires a public_key.json, a private_uciv.json and a public_uciv.json in the project root. These can be generated using generator-rs.","items":[],"paths":[]};
+searchIndex["client_rs"] = {"doc":"This client provides an easy way to submit a vote to the blockchain. To generate all required cryptographic proofs, it requires a `public_key.json`, a `private_uciv.json` and a `public_uciv.json` in the same folder as the binary is executed.","items":[],"paths":[]};
 initSearch(searchIndex);
